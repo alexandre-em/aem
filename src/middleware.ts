089ac788def0f66c103874dda23d28392bd28d14
Alexandre <em_alexandre@yahoo.co.jp>
2024-04-05 23:08:29 +0200

    Add i18n and setup on project

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import createMiddleware from 'next-intl/middleware';
+
+import { localePrefix, locales } from './navigation';
+
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales,
+  localePrefix,
+
+  // Used when no locale matches
+  defaultLocale: 'en',
+});
+
+export const config = {
+  matcher: ['/', '/(fr|en|jp)?/blog/(.+)?', '/(fr|en|jp)?/projects/(.+)?', '/(fr|en|jp)?/gallery', '/(fr|en|jp)?/dashboard/(.+)'],
+};
---
+++ src/navigation.ts
@@ -0,0 +1,9 @@
+import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+
+export const locales = ['en', 'fr', 'jp'] as const;
+export const localePrefix = 'as-needed'; // Default
+
+export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation({
+  locales,
+  localePrefix,
+});
