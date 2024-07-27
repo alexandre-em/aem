19a0d839decf9bc50caaec53d7b5d51e61bbddc6
Alexandre <em_alexandre@yahoo.co.jp>
2024-07-27 22:51:13 +0200

    Update markdown reader for quote

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -6,12 +6,17 @@ export const Pre = ({ ...props }) => {
 };
 
 export const CodeBlock = ({ ...props }) => {
-  return (
-    <SyntaxHighlighter
-      language={props.className?.replace(/(?:lang(?:uage)?-)/, '')}
-      style={oneDark}
-      className="not-prose rounded-md w-[calc(100dvw-45px)] sm:w-full overflow-auto">
-      {props.children}
-    </SyntaxHighlighter>
-  );
+  const match = /language-(\w+)/.exec(props.className || '');
+
+  if (!props.inline && match)
+    return (
+      <SyntaxHighlighter
+        language={props.className?.replace(/(?:lang(?:uage)?-)/, '')}
+        style={oneDark}
+        className="not-prose rounded-md w-[calc(100dvw-45px)] sm:w-full overflow-auto">
+        {props.children}
+      </SyntaxHighlighter>
+    );
+
+  return <code className="bg-primary-foreground border-2 border-primary-foreground rounded-md">{props.children}</code>;
 };
