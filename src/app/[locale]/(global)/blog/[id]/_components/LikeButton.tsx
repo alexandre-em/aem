fa1eec5b7ec347089c412b997ee68d4d77fa7494
Alexandre <em_alexandre@yahoo.co.jp>
2024-07-29 16:34:48 +0200

    Add toast on evaluation (blog)

--- src/app/[locale]/(global)/blog/[id]/_components/LikeButton.tsx
+++ src/app/[locale]/(global)/blog/[id]/_components/LikeButton.tsx
@@ -3,14 +3,27 @@ import { HeartIcon } from 'lucide-react';
 import React, { useCallback } from 'react';
 
 import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/use-toast';
 import { BlogService } from '@/services';
 
 export default function LikeButton({ id, like }: { id: string; like: number }) {
+  const { toast } = useToast();
+
   const handleLike = useCallback(() => {
     if (id) {
-      BlogService.incrementLike(id);
+      BlogService.incrementLike(id)
+        .then(() => {
+          toast({ title: 'Thank you for the support !', description: 'Evaluation sent successfuly', variant: 'success' });
+        })
+        .catch(() =>
+          toast({
+            title: 'An error occured. Please try again later...',
+            description: 'Evaluation failed',
+            variant: 'destructive',
+          })
+        );
     }
-  }, [id]);
+  }, [id, toast]);
 
   return (
     <Button variant="ghost" className="rounded-full font-bold" onClick={handleLike}>
